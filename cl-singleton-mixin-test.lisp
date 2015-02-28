6642e057797dc164edc4d0ac11f4a040548b864e
hipeta <rhipeta@gmail.com>
2015-02-28 16:30:25 +0900

    Add singleton tests

--- cl-singleton-mixin-test.lisp
+++ cl-singleton-mixin-test.lisp
@@ -11,3 +11,26 @@
 (in-package :cl-singleton-mixin-test)
 
 (in-suite* all)
+
+(defclass a (singleton-mixin) ())
+(defclass b (a) ())
+(defclass c (b) ())
+(defclass d (a) ())
+(defclass e (c d) ())
+
+(test singleton-test
+  (is (eq (make-instance 'a) (make-instance 'a)))
+  (is (eq (make-instance 'b) (make-instance 'b)))
+  (is (eq (make-instance 'c) (make-instance 'c)))
+  (is (eq (make-instance 'd) (make-instance 'd)))
+  (is (eq (make-instance 'e) (make-instance 'e)))
+  (is (not (eq (make-instance 'a) (make-instance 'b))))
+  (is (not (eq (make-instance 'a) (make-instance 'c))))
+  (is (not (eq (make-instance 'a) (make-instance 'd))))
+  (is (not (eq (make-instance 'a) (make-instance 'e))))
+  (is (not (eq (make-instance 'b) (make-instance 'c))))
+  (is (not (eq (make-instance 'b) (make-instance 'd))))
+  (is (not (eq (make-instance 'b) (make-instance 'e))))
+  (is (not (eq (make-instance 'c) (make-instance 'd))))
+  (is (not (eq (make-instance 'c) (make-instance 'e))))
+  (is (not (eq (make-instance 'd) (make-instance 'e)))))
